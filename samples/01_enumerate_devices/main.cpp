4f7e766bc825fba41efa62306d92dc5f1f9d1a6a
Andrey Viktorov <andrey.viktorov@gmail.com>
2025-03-15 23:59:18 +0700

    [Samples] Updated device enumeration sample with all instance level functionality

--- samples/01_enumerate_devices/main.cpp
+++ samples/01_enumerate_devices/main.cpp
@@ -2,36 +2,42 @@
 #include <cassert>
 #include <iostream>
 
-void printDeviceInfo(const Quartz_DeviceInfo *info)
+static const char *device_types[] =
+{
+	"Playback",
+	"Recording",
+};
+
+static void printDeviceInfo(const Quartz_DeviceInfo *info)
 {
 	std::cout << "Device name: " << info->name << "\n";
 	std::cout << "\n";
 }
 
-void testEnumerateDevices(Quartz_Instance instance)
+static void testEnumerateDevices(Quartz_Instance instance, Quartz_DeviceType type)
 {
 	uint32_t device_count = 0;
-	Quartz_Result result = quartzEnumerateDevices(instance, &device_count, nullptr);
+	Quartz_Result result = quartzEnumerateDevices(instance, type, &device_count, nullptr);
 	if (result != QUARTZ_SUCCESS)
 		return;
 
 	Quartz_DeviceInfo *infos = (Quartz_DeviceInfo *)malloc(sizeof(Quartz_DeviceInfo) * device_count);
-	result = quartzEnumerateDevices(instance, &device_count, infos);
+	result = quartzEnumerateDevices(instance, type, &device_count, infos);
 	assert(result == QUARTZ_SUCCESS);
 
 	for (uint32_t i = 0; i < device_count; ++i)
 	{
-		std::cout << " ------ [Enumerate " << i << "] ------\n";
+		std::cout << " ------ [Device " << i << "] ------\n";
 		printDeviceInfo(&infos[i]);
 	}
 
 	free(infos);
 }
 
-void testCreateDevice(Quartz_Instance instance, uint32_t index)
+static void testCreateDevice(Quartz_Instance instance, Quartz_DeviceType type, uint32_t index)
 {
 	Quartz_Device device = QUARTZ_NULL_HANDLE;
-	Quartz_Result result = quartzCreateDevice(instance, index, &device);
+	Quartz_Result result = quartzCreateDevice(instance, type, index, &device);
 	if (result != QUARTZ_SUCCESS)
 		return;
 
@@ -39,6 +45,23 @@ void testCreateDevice(Quartz_Instance instance, uint32_t index)
 	result = quartzGetDeviceInfo(device, &info);
 	assert(result == QUARTZ_SUCCESS);
 	std::cout << " ------ [CreateByIndex " << index << "] ------\n";
+	printDeviceInfo(&info);
+
+	result = quartzDestroyDevice(device);
+	assert(result == QUARTZ_SUCCESS);
+}
+
+static void testCreateDefaultDevice(Quartz_Instance instance, Quartz_DeviceType type)
+{
+	Quartz_Device device = QUARTZ_NULL_HANDLE;
+	Quartz_Result result = quartzCreateDefaultDevice(instance, type, &device);
+	if (result != QUARTZ_SUCCESS)
+		return;
+
+	Quartz_DeviceInfo info = {};
+	result = quartzGetDeviceInfo(device, &info);
+	assert(result == QUARTZ_SUCCESS);
+	std::cout << " ------ [CreateDefault] ------\n";
 	printDeviceInfo(&info);
 
 	result = quartzDestroyDevice(device);
@@ -60,8 +83,23 @@ int main()
 	Quartz_Result result = quartzCreateInstance(QUARTZ_API_WASAPI, &instance_desc, &instance);
 	assert(result == QUARTZ_SUCCESS);
 
-	testEnumerateDevices(instance);
-	testCreateDevice(instance, 0);
+	Quartz_DeviceType types[] =
+	{
+		QUARTZ_DEVICE_TYPE_PLAYBACK,
+		QUARTZ_DEVICE_TYPE_RECORDING,
+	};
+
+	const size_t count = std::size(types);
+
+	for (size_t i = 0; i < count; ++i)
+	{
+		Quartz_DeviceType type = types[i];
+
+		std::cout << " ------ [" << device_types[type] << " devices] ------\n";
+		testEnumerateDevices(instance, type);
+		testCreateDevice(instance, type, 0);
+		testCreateDefaultDevice(instance, type);
+	}
 
 	result = quartzDestroyInstance(instance);
 	assert(result == QUARTZ_SUCCESS);
