354ff81893fdf4bc07bb4ec0db3978486225d2e5
Andrey Viktorov <andrey.viktorov@gmail.com>
2025-07-25 01:11:11 +0700

    [Samples] Updated buffer sample to showcase sine wave rendering

--- samples/02_buffers/main.cpp
+++ samples/02_buffers/main.cpp
@@ -1,27 +1,68 @@
 #include <quartz.h>
+
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <cassert>
+
 #include <iostream>
 
 static void testBuffers(Quartz_Device device)
 {
 	Quartz_Buffer buffer = QUARTZ_NULL_HANDLE;
-	void *ptr = nullptr;
 
 	Quartz_BufferDesc desc =
 	{
 		QUARTZ_SAMPLE_RATE_48000,
-		QUARTZ_SAMPLE_FORMAT_FLOAT32,
-		1000,
+		QUARTZ_SAMPLE_FORMAT_SINT32,
+		50,
 		2
 	};
 
 	Quartz_Result result = quartzCreateBuffer(device, &desc, &buffer);
 	assert(result == QUARTZ_SUCCESS);
 
-	result = quartzMapBuffer(device, buffer, &ptr);
+	result = quartzStart(device, buffer);
 	assert(result == QUARTZ_SUCCESS);
 
-	result = quartzUnmapBuffer(device, buffer);
+	int32_t *frames = nullptr;
+	uint32_t frame_count = 0;
+
+	double phase1 = 0.0;
+	double phase2 = 0.0;
+	const double freq1 = 440.0;
+	const double freq2 = 441.0;
+	const double amp = 0.5;
+	const double inc1 = 2.0 * M_PI * freq1 / desc.sample_rate;
+	const double inc2 = 2.0 * M_PI * freq2 / desc.sample_rate;
+
+	while (true)
+	{
+		result = quartzBeginRender(device, buffer, reinterpret_cast<void **>(&frames), &frame_count);
+		assert(result == QUARTZ_SUCCESS);
+
+		if (frame_count > 0)
+			std::cout << "Rendering " << frame_count << " frames\n";
+
+		for (uint32_t i = 0; i < frame_count; ++i)
+		{
+			phase1 += inc1;
+			phase2 += inc2;
+
+			double osc1 = sin(phase1) * amp;
+			double osc2 = sin(phase2) * amp;
+
+			int32_t sample1 = static_cast<int32_t>(osc1 * INT32_MAX);
+			int32_t sample2 = static_cast<int32_t>(osc2 * INT32_MAX);
+
+			frames[2 * i + 0] = sample1;
+			frames[2 * i + 1] = sample2;
+		}
+
+		result = quartzEndRender(device, buffer, frame_count);
+		assert(result == QUARTZ_SUCCESS);
+	}
+
+	result = quartzStop(device, buffer);
 	assert(result == QUARTZ_SUCCESS);
 
 	result = quartzDestroyBuffer(device, buffer);
@@ -44,7 +85,7 @@ int main()
 	assert(result == QUARTZ_SUCCESS);
 
 	Quartz_Device device = QUARTZ_NULL_HANDLE;
-	result = quartzCreateDefaultDevice(instance, QUARTZ_DEVICE_TYPE_PLAYBACK, &device);
+	result = quartzCreateDefaultDevice(instance, QUARTZ_DEVICE_TYPE_RENDER, &device);
 	assert(result == QUARTZ_SUCCESS);
 
 	testBuffers(device);
